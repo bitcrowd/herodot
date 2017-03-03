9b3ec14db5006079a3865562215f0f588b16ff1b
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2017-03-03 15:09:26 +0100

    dump and load configuration from dotfile

--- lib/herodot/configuration.rb
+++ lib/herodot/configuration.rb
@@ -1,14 +1,25 @@
+require 'yaml'
+
 class Herodot::Configuration
-  CONFIG_FILE = '~/.herodot'.freeze
-  def initialize
-    @projects_directory = '~'
-    @worklog_file = '~/worklog'
-    @work_times = {
-      work_start: '9:30',
-      lunch_break_start: '13:00',
-      lunch_break_end: '13:30',
-      work_end: '18:00'
+  CONFIG_FILE = File.expand_path('~/.herodot').freeze
+  DEFAULT_CONFIGURATION = {
+    'projects_directory' => '~',
+    'work_times' => {
+      'work_start' => '9:30',
+      'lunch_break_start' => '13:00',
+      'lunch_break_end' => '13:30',
+      'work_end' => '18:00'
     }
+  }.freeze
+
+  def initialize
+    @worklog_file = '~/worklog'
+    if File.exist?(CONFIG_FILE)
+      @config = load_configuration
+    else
+      @config = DEFAULT_CONFIGURATION
+      save_configuration
+    end
   end
 
   def worklog_file
@@ -16,10 +27,18 @@ class Herodot::Configuration
   end
 
   def projects_directory
-    File.expand_path(@projects_directory)
+    File.expand_path(@config['projects_directory'])
   end
 
   def work_times
-    @work_times.map { |k, v| [k, v.split(':').map(&:to_i)] }
+    @config['work_times'].map { |k, v| [k.to_sym, v.split(':').map(&:to_i)] }
+  end
+
+  def save_configuration
+    File.open(CONFIG_FILE, 'w') { |f| YAML.dump(@config, f) }
+  end
+
+  def load_configuration
+    File.open(CONFIG_FILE) { |f| YAML.safe_load(f) }
   end
 end
